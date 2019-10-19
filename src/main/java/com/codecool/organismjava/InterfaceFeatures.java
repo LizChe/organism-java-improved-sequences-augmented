89f3511bc92390940a8d3e2b8341d88b86e36c45
Liz <pachaffy@gmail.com>
2019-10-19 21:58:18 +0200

    Implement InterfaceFeatures

--- src/main/java/com/codecool/organismjava/InterfaceFeatures.java
+++ src/main/java/com/codecool/organismjava/InterfaceFeatures.java
@@ -1,4 +1,25 @@
 package com.codecool.organismjava;
 
 public interface InterfaceFeatures {
+
+    private String someExtremelyImportantCalculation(int a, int b) {
+        return a + b + " private methods in interfaces are possible.";
+    }
+
+    default void doCalculation(int a, int b) {
+        System.out.println(someExtremelyImportantCalculation(a, b));
+    }
+
+    default void doAnotherCalculation(int a, int b) {
+        System.out.println(someExtremelyImportantCalculation(a, b));
+    }
+}
+
+class Features implements InterfaceFeatures {
+
+    public static void main(String[] args) {
+        InterfaceFeatures features = new Features();
+        features.doCalculation(1, 1);
+        features.doAnotherCalculation(2, 2);
+    }
 }
