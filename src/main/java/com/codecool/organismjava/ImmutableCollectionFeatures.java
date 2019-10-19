fd5d96f6660f9f4743833c86e8c7c047df35a95d
Liz <pachaffy@gmail.com>
2019-10-19 22:24:29 +0200

    Implement ImmutableCollectionFeatures

--- src/main/java/com/codecool/organismjava/ImmutableCollectionFeatures.java
+++ src/main/java/com/codecool/organismjava/ImmutableCollectionFeatures.java
@@ -1,4 +1,34 @@
 package com.codecool.organismjava;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ImmutableCollectionFeatures {
+
+    /**
+     * Before Java 9
+     *
+     * @return unmodifiable set, list, map, etc. using Collections.
+     */
+    private Set<String> createImmutableSet() {
+        Set<String> stringSet = new HashSet<>();
+        stringSet.add("Why you are not using Java 9?");
+        return Collections.unmodifiableSet(stringSet);
+    }
+
+    /**
+     * Since Java 9
+     *
+     * @return unmodifiable set, list, map, etc. in one line.
+     */
+
+    private Set<String> createImmutableSetInEasyWay() {
+        return Set.of("Java 9 is awesome!");
+    }
+
+    private List<String> createImmutableList() {
+        return List.of("Unmodifiable List");
+    }
 }
