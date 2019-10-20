0c1d42b91039bd781e1aeb7912d3e4a48cc8080f
Liz <pachaffy@gmail.com>
2019-10-20 22:43:09 +0200

    Implement CompileMe

--- src/main/java/com/codecool/organismjava/CompileMe.java
+++ src/main/java/com/codecool/organismjava/CompileMe.java
@@ -1,4 +1,10 @@
 package com.codecool.organismjava;
 
 public class CompileMe {
+
+    public static void main(String[] args) {
+        System.out.println("Remember when you had to use "
+                + "`javac CompileMe.java` first and then "
+                + "`java CompileMe` to run a single file?");
+    }
 }
