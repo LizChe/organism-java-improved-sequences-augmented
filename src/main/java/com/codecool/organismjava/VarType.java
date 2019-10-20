fdae6f5eb5ccead101778a848759dd3742643856
Liz <pachaffy@gmail.com>
2019-10-20 22:02:58 +0200

    Rename VarKeyword to VarType

--- src/main/java/com/codecool/organismjava/VarType.java
+++ src/main/java/com/codecool/organismjava/VarType.java
@@ -5,28 +5,4 @@ import java.util.HashMap;
 
 public class VarType {
 
-    /**
-     * Since Java 10 we can use Local-Variable Type Inference
-     * When vas is used, compiler checks what we have on the right side
-     * and infers the type. Var has to be initialized from the start.
-     *
-     * Important things to know:
-     *  - We can't use var keyword as a field
-     *  - We can't use var keyword as a return type in methods
-     *  -
-     */
-
-    public static void main(String[] args) {
-        var hello = "Hello var keyword!";
-        var map = new HashMap<>();
-        var list = new ArrayList<>();
-
-
-        list.add("apples");
-        list.add(1);
-
-        for (var thing : list) {
-            System.out.println(thing.getClass());
-        }
-    }
 }
