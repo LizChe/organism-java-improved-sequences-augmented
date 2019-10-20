28bf828425c14405b712f127819c059cac010b8a
Liz <pachaffy@gmail.com>
2019-10-20 23:00:56 +0200

    Implement FilesFeatures

--- src/main/java/com/codecool/organismjava/FilesFeatures.java
+++ src/main/java/com/codecool/organismjava/FilesFeatures.java
@@ -1,4 +1,23 @@
 package com.codecool.organismjava;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FilesFeatures {
+
+    private void writeStringMethod() throws IOException {
+        Files.writeString(Path.of("src/main/resources/files/test.txt"), "Hello to Java 11 features!");
+    }
+
+    private void readStringMethod() throws IOException {
+        String fileText = Files.readString(Path.of("src/main/resources/files/test.txt"));
+        System.out.println(fileText);
+    }
+
+    public static void main(String[] args) throws IOException {
+        FilesFeatures filesFeatures = new FilesFeatures();
+        filesFeatures.writeStringMethod();
+        filesFeatures.readStringMethod();
+    }
 }
